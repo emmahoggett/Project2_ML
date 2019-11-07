45b50c4befb9457b84cf47c0fe44e4a721a98fb0
emmahoggett <emma.hoggett@epfl.ch>
2019-11-07 14:41:18 +0100

    File with RMSE and MSE

--- RMSE.py
+++ RMSE.py
@@ -1,2 +1,9 @@
-def RMSE(valid_ratings, train, test):
-    
+
+def RMSE(mse,test):
+    """"calculate RMSE"""
+    return np.sqrt(1.0 * mse / test.nnz)
+
+def calculate_mse(real_label, prediction):
+    """calculate MSE."""
+    t = real_label - prediction
+    return 1.0 * t.dot(t.T)
