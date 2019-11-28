b7ff10de2be3d147da36f328720e39e38be3028a
emmahoggett <emma.hoggett@epfl.ch>
2019-11-28 15:25:09 +0100

    Finished CoClustering/ Erase convert_train in helpers

--- 2_DotProduct/helpers.py
+++ 2_DotProduct/helpers.py
@@ -37,20 +37,4 @@ def calculate_mse(real_label, prediction):
     t = real_label - prediction
     return 1.0 * t.dot(t.T)
 
-def convert_train(ratings):
-    """convert sparse rating matrix into a data frame"""
-    #convert ratings into an array
-    matrix_0 = ratings.toarray()
-    matrix_1 = matrix_0.flatten()[matrix_0.flatten().nonzero()]
-    
-    #concatenate user_id, movie_id and ratings
-    matrix_2 = np.vstack((matrix_0.nonzero()[0],matrix_0.nonzero()[1],matrix_1))
-    matrix_3 = matrix_2.T.astype(int)
-    
-    # convert rating into a data frame
-    ratings = pd.DataFrame({'user_id': matrix_3[:,0], 'movie_id': matrix_3[:,1],'rating':matrix_3[:,2]})
-    n_users = len(ratings.user_id.unique())
-    n_movies = len(ratings.movie_id.unique())
-    
-    return ratings, n_users, n_movies
 
